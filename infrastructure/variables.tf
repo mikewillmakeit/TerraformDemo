e14d7841fb6219fea4d73846974ee5ff51ee1ed2
mikewillmakeit <mikenatsfan@gmail.com>
2019-07-26 08:44:23 -0400

    changes

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -9,25 +9,31 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_1_cidr" {
+  default = "10.0.1.0/16"
   description = "Public Subnet 1 CIDR"
 }
 
 variable "public_subnet_2_cidr" {
+   default = "10.0.2.0/16"
   description = "Public Subnet 2 CIDR"
 }
 
 variable "public_subnet_3_cidr" {
+   default = "10.0.3.0/16"
   description = "Public Subnet 3 CIDR"
 }
 
 variable "private_subnet_1_cidr" {
+   default = "10.0.4.0/16"
   description = "Private Subnet 1 CIDR"
 }
 
 variable "private_subnet_2_cidr" {
+   default = "10.0.5.0/16"
   description = "Private Subnet 2 CIDR"
 }
 
 variable "private_subnet_3_cidr" {
+   default = "10.0.6.0/16"
   description = "Private Subnet 3 CIDR"
 }
